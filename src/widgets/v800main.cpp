40cd3356ee079aeab2f356b99cba308ece4c9879
lasdou <florian15@gmail.com>
2018-05-22 15:40:47 +0200

    Update v800main.cpp
    adding V650 support

--- src/widgets/v800main.cpp
+++ src/widgets/v800main.cpp
@@ -83,6 +83,7 @@ V800Main::V800Main(QWidget *parent) :
     QStringList devices;
     devices.append(tr("V800"));
     devices.append(tr("M400"));
+    devices.append(tr("V650"));
 
     bool ok;
     QString selected_device;
@@ -99,6 +100,8 @@ V800Main::V800Main(QWidget *parent) :
         usb = new V800usb(V800);
     else if(selected_device == tr("M400"))
         usb = new V800usb(M400);
+    else if(selected_device == tr("V650"))
+        usb = new V800usb(V650);
     usb->moveToThread(usb_thread);
 
     connect(usb, SIGNAL(all_sessions(QList<QString>)), this, SLOT(handle_all_sessions(QList<QString>)));
